1bfe0bfe94cd9c0958346a48bd1ffce7c8c236ef
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-04-06 00:54:41 +0200

    Disable reply guest buttons when login is disabled

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,15 +1,70 @@
+import {Child} from 'mithril';
 import app from 'flarum/forum/app';
 import {extend} from 'flarum/common/extend';
 import SettingsPage from 'flarum/forum/components/SettingsPage';
 import HeaderSecondary from 'flarum/forum/components/HeaderSecondary';
 import UserCard from 'flarum/forum/components/UserCard';
 import LogInModal from 'flarum/forum/components/LogInModal';
+import IndexPage from 'flarum/forum/components/IndexPage';
+import PostStream from 'flarum/forum/components/PostStream';
+import DiscussionControls from 'flarum/forum/utils/DiscussionControls';
 
 app.initializers.add('readonly-profile', () => {
     extend(HeaderSecondary.prototype, 'items', function (items) {
         if (app.forum.attribute('readonly-profile.disableLogin')) {
             items.remove('logIn');
         }
+    });
+
+    extend(IndexPage.prototype, 'sidebarItems', function (items) {
+        if (
+            !app.forum.attribute('readonly-profile.disableLogin') ||
+            !items.has('newDiscussion') ||
+            app.session.user
+        ) {
+            return;
+        }
+
+        const newDiscussionVdom = items.get('newDiscussion');
+
+        // Force "new discussion" button to show "can't start discussion" for guests
+        // because the default behaviour was to open the login modal which is no longer available
+        newDiscussionVdom.attrs.disabled = true;
+        newDiscussionVdom.children = app.translator.trans('core.forum.index.cannot_start_discussion_button');
+    });
+
+    extend(PostStream.prototype, 'view', function (vdom) {
+        if (!vdom || !Array.isArray(vdom.children)) {
+            // If the data format is unexpected, skip this feature
+            return;
+        }
+
+        if (!app.forum.attribute('readonly-profile.disableLogin') || app.session.user) {
+            // If we are not disabling login or if the user is already logged in, skip
+            return;
+        }
+
+        // Remove the .PostStream-item containing ReplyPlaceholder
+        const index = (vdom.children as Child[]).findIndex(item => item && typeof item === 'object' && item.attrs && item.attrs.key === 'reply');
+
+        if (index !== -1) {
+            vdom.children.splice(index, 1);
+        }
+    });
+
+    extend(DiscussionControls, 'userControls', function (items) {
+        if (
+            !app.forum.attribute('readonly-profile.disableLogin') ||
+            !items.has('reply') ||
+            app.session.user
+        ) {
+            return;
+        }
+
+        const replyVdom = items.get('reply');
+
+        // Keep the original button that says login to reply, but disable it
+        replyVdom.attrs.disabled = true;
     });
 
     extend(SettingsPage.prototype, 'accountItems', function (items) {
