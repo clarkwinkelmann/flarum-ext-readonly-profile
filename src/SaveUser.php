d4361328a93bf95523ef29e3798bbe7312cee832
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-09-06 17:11:25 +0200

    Fix disableEmailChange always applying even when disabled

--- src/SaveUser.php
+++ src/SaveUser.php
@@ -4,6 +4,7 @@ namespace ClarkWinkelmann\ReadonlyProfile;
 
 use Flarum\Foundation\ValidationException;
 use Flarum\Locale\Translator;
+use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\User\Event\Saving;
 use Illuminate\Support\Arr;
 
@@ -14,6 +15,15 @@ class SaveUser
         $attributes = Arr::get($event->data, 'attributes', []);
 
         if ($event->user->exists && array_key_exists('email', $attributes) && !$event->actor->isAdmin()) {
+            /**
+             * @var $settings SettingsRepositoryInterface
+             */
+            $settings = resolve(SettingsRepositoryInterface::class);
+
+            if (!$settings->get('readonly-profile.disableEmailChange')) {
+                return;
+            }
+
             /**
              * @var $translator Translator
              */
